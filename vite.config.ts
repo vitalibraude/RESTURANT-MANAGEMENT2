66e9cbf416d33b295375ec939e263a1c1afda61f
vitali pavlovski <vitalibraude@gmail.com>
2026-02-02 22:06:56 +0000

    Configure for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/RESTURANT-MANAGEMENT2/',
+      base: './',
       server: {
         port: 3001,
         host: '0.0.0.0',
