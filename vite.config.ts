60cc930ae504904d24c63cec05b6092e389819ce
vitali pavlovski <vitalibraude@gmail.com>
2026-02-02 21:46:54 +0000

    Update base path for new repo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/RESTURANT-MANAGEMENT/',
+      base: '/RESTURANT-MANAGEMENT2/',
       server: {
         port: 3001,
         host: '0.0.0.0',
